ad62d2de5acf266d085215da65ec281c082ddbfe
baeksu <kyungwoo5826@naver.com>
2024-03-28 23:59:38 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-import { useEffect, useState } from "react";
-
-function fetchTodos() {
-    const todos = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
-        todos.push(value);
-    }
-    return todos;
-}
-
 function App() {}
 
 export default App;
